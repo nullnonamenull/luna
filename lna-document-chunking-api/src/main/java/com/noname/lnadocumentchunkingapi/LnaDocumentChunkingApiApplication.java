ecc0ac0864a556a412f4b3d745126de6d5133e71
Piotras <poleszak@protonmail.com>
2024-11-19 19:41:29 +0100

    Add chunking module

---
+++ lna-document-chunking-api/src/main/java/com/noname/lnadocumentchunkingapi/LnaDocumentChunkingApiApplication.java
@@ -0,0 +1,13 @@
+package com.noname.lnadocumentchunkingapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LnaDocumentChunkingApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LnaDocumentChunkingApiApplication.class, args);
+    }
+
+}
