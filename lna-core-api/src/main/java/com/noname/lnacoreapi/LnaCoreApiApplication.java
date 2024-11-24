5ad52ec9ba2b5c2168bc091600246f1aadc266ef
Piotras <poleszak@protonmail.com>
2024-11-24 19:12:47 +0100

    Add lna-core-api

---
+++ lna-core-api/src/main/java/com/noname/lnacoreapi/LnaCoreApiApplication.java
@@ -0,0 +1,13 @@
+package com.noname.lnacoreapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LnaCoreApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LnaCoreApiApplication.class, args);
+	}
+
+}
